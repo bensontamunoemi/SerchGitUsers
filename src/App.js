9f7417cf51039564728ca5d37ed6df53785fa93d
massive5566 <bensontamunoemi@gmail.com>
2021-03-25 01:55:44 +0400

    Moved  search users to context state

--- src/App.js
+++ src/App.js
@@ -19,28 +19,18 @@ const App = () => {
 	const [loading, setLoading] = useState(false);
 	const [alert, setAlert] = useState(null);
 
-	useEffect(() => {
-		const myFetchApi = async () => {
-			setLoading(true);
-			const res = await axios.get(
-				`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-			);
-			setUsers(res.data);
-			setLoading(false);
-		};
-		myFetchApi();
-		// eslint-disable-next-line
-	}, []);
-
-	// search Users
-	const searchUsers = async text => {
-		setLoading(true);
-		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-		);
-		setUsers(res.data.items);
-		setLoading(false);
-	};
+	// useEffect(() => {
+	// 	const myFetchApi = async () => {
+	// 		setLoading(true);
+	// 		const res = await axios.get(
+	// 			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+	// 		);
+	// 		setUsers(res.data);
+	// 		setLoading(false);
+	// 	};
+	// 	myFetchApi();
+	// 	// eslint-disable-next-line
+	// }, []);
 
 	// Get Single User
 	const getUser = async username => {
@@ -74,46 +64,47 @@ const App = () => {
 	};
 
 	return (
-		<Router>
-			<div className='App'>
-				<Navbar title='Github Finder' icon='fab fa-github' />
-				<div className='container'>
-					<Alert alert={alert} />
-					<Switch>
-						<Route
-							exact
-							path='/'
-							render={props => (
-								<Fragment>
-									<Search
-										searchUsers={searchUsers}
-										clearUsers={clearUsers}
-										showClear={users.length > 0 ? true : false}
-										setAlert={showAlert}
+		<GithubState>
+			<Router>
+				<div className='App'>
+					<Navbar title='Github Finder' icon='fab fa-github' />
+					<div className='container'>
+						<Alert alert={alert} />
+						<Switch>
+							<Route
+								exact
+								path='/'
+								render={props => (
+									<Fragment>
+										<Search
+											clearUsers={clearUsers}
+											showClear={users.length > 0 ? true : false}
+											setAlert={showAlert}
+										/>
+										<Users loading={loading} users={users} />
+									</Fragment>
+								)}
+							/>
+							<Route exact path='/about' component={About} />
+							<Route
+								exact
+								path='/user/:login'
+								render={props => (
+									<User
+										{...props}
+										getUser={getUser}
+										getUserRepos={getUserRepos}
+										repos={repos}
+										user={user}
+										loading={loading}
 									/>
-									<Users loading={loading} users={users} />
-								</Fragment>
-							)}
-						/>
-						<Route exact path='/about' component={About} />
-						<Route
-							exact
-							path='/user/:login'
-							render={props => (
-								<User
-									{...props}
-									getUser={getUser}
-									getUserRepos={getUserRepos}
-									repos={repos}
-									user={user}
-									loading={loading}
-								/>
-							)}
-						/>
-					</Switch>
+								)}
+							/>
+						</Switch>
+					</div>
 				</div>
-			</div>
-		</Router>
+			</Router>
+		</GithubState>
 	);
 };
 
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -20,24 +20,32 @@ const GithubState = props => {
 
 	const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-	// SEARCH_USERS
-	// GET_USER
-	// CLEAR_USERS
-	// GET_REPOS
-	// SET_LOADING
+	// SEARCH_USERS,
+	const searchUsers = async text => {
+		setLoading();
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		dispatch({ type: SEARCH_USERS, payload: res.data.items });
+	};
 
-	// Return provider
+	// SET_LOADING,
+	const setLoading = () => dispatch({ type: SET_LOADING });
 
-	<GithubContext.Provider
-		value={{
-			users: state.users,
-			user: state.user,
-			repos: state.repos,
-			loading: state.loading,
-		}}
-	>
-		{props.children}
-	</GithubContext.Provider>;
+	// Return Provider
+	return (
+		<GithubContext.Provider
+			value={{
+				users: state.users,
+				user: state.user,
+				repos: state.repos,
+				loading: state.loading,
+				searchUsers,
+			}}
+		>
+			{props.children}
+		</GithubContext.Provider>
+	);
 };
 
 export default GithubState;
